32dc60fb6134f36ad3b2f4483c9306d1e94d5972
ozubenko-softheme <oleg.zubenko@softheme.com>
2015-08-26 18:56:18 +0300

    HW_CSharp_21_08

---
+++ Delegates_Task1/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+
+namespace HW_Delegates
+{
+    class Program
+    {
+        static void Main()
+        {
+            MyCalculator.Calculator();
+            Console.ReadKey();
+        }
+    }
+
+    static class MyCalculator
+    {
+        delegate double MyDelegate(double op1, double op2);
+
+        static MyDelegate addition = (op1, op2) =>
+        {
+            return op1 + op2;
+        };
+
+        static MyDelegate substraction = (op1, op2) =>
+        {
+            return op1 - op2;
+        };
+
+        static MyDelegate multiplication = (op1, op2) =>
+        {
+            return op1 * op2;
+        };
+
+        static MyDelegate division = (op1, op2) =>
+        {
+            if (op2 == 0)
+            {
+                Console.WriteLine("Division by zero!");
+            }
+            return op1 / op2;
+        };
+
+        public static double Calculate(double op1, double op2, char oprtr)
+        {
+            double result = 0;
+            switch (oprtr)
+            {
+                case '+':
+                    result = addition(op1, op2);
+                    break;
+                case '-':
+                    result = substraction(op1, op2);
+                    break;
+                case '*':
+                    result = multiplication(op1, op2);
+                    break;
+                case '/':
+                    result = division(op1, op2);
+                    break;
+                default:
+                    Console.WriteLine("Can't calculate!");
+                    break;
+            }
+            return result;
+        }
+
+        public static void Calculator()
+        {
+            double operand1 = 0;
+            double operand2 = 0;
+            double result = 0;
+            char oprtr = ' ';
+
+            try
+            {
+                Console.Write("Enter operand1: ");
+                operand1 = Double.Parse(Console.ReadLine());
+                Console.Write("\nEnter operator: ");
+                oprtr = Char.Parse(Console.ReadLine());
+                Console.Write("\nEnter operand2: ");
+                operand2 = Double.Parse(Console.ReadLine());
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            result = Calculate(operand1, operand2, oprtr);
+            if (result != Double.PositiveInfinity & result != Double.NegativeInfinity)
+                Console.WriteLine("\nResult: " + result);
+        }
+    }
+}
---
+++ Delegates_Task2/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace HW_Delegates
+{
+    internal delegate int MyDelegate(MyDelegate2[] delegates);
+
+    internal delegate int MyDelegate2();
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDelegate2 method1 = MyMethods.Method1;
+            MyDelegate2 method2 = MyMethods.Method2;
+            MyDelegate2 method3 = MyMethods.Method3;
+            MyDelegate2[] delegates = new MyDelegate2[3];
+            delegates[0] = method1;
+            delegates[1] = method2;
+            delegates[2] = method3;
+            MyDelegate del = delegate(MyDelegate2[] dels)
+            {
+                int sum = 0;
+                for (int i = 0; i < dels.Length; i++)
+                {
+                    sum += dels[i]();
+                    MyDelegate2 d = dels[i];
+                }
+                return sum / delegates.Length;
+            };
+
+            int result = del(delegates);
+            Console.WriteLine("Arifmetic average:" + result);
+            Console.ReadKey();
+        }
+    }
+
+    static class MyMethods
+    {
+        public static int Method1()
+        {
+            int val;
+            Random rand = new Random();
+            val = rand.Next(10) + 1;
+            Console.WriteLine(val);
+            return val;
+        }
+
+        public static int Method2()
+        {
+            int val;
+            Random rand = new Random();
+            val = rand.Next(20) + 1;
+            Console.WriteLine(val);
+            return val;
+        }
+
+        public static int Method3()
+        {
+            int val;
+            Random rand = new Random();
+            val = rand.Next(30) + 1;
+            Console.WriteLine(val);
+            return val;
+        }
+    }
+}
